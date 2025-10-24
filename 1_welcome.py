de0b827e34ef25d7068d673952b45da1f49fd07b
Anoop Bose <63749480+anoopboseml@users.noreply.github.com>
2025-10-24 10:15:22 +0530

    Add files via upload
    added file 1 and file 2

---
+++ 1_welcome.py
@@ -0,0 +1,11 @@
+greetings = "Hello World"
+
+print(greetings)
+
+
+# We can run this python file in 3 ways
+
+# Method 1 - Use the Play button on the top right corner
+# Method 2 - Use the CTRL+ENTER command
+# Method 3 - In the command terminal enter Python <full case sensitive file name> 
+# this assumes that you are already in the same folder where your file is living
---
+++ 2_variableNames.py
@@ -0,0 +1,31 @@
+# variable names
+
+_name = "Anoop"  # this is valid
+
+# this is invalid variable name!
+# this is also invalid name-name
+
+# we can only use numbers, letters and _
+# var names can not begin with numbers
+# ew also can not use reserved words as var names
+##########################################################################
+
+# to copy a line of code to the next line use SHIFT+ALT+DOWNARROW
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+line02 = "*                  *"
+line01 = "********************"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+
+########################################################################
+
+# INDENTATION
